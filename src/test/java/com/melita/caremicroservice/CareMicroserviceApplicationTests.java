c6dd8bab994e238dd37f063699cd7d065b45c749
Geza Czimeth <geza.czimeth@gmail.com>
2023-01-28 16:58:57 +0100

    Initial setup of RabbitMQ

---
+++ src/test/java/com/melita/caremicroservice/CareMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melita.caremicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
